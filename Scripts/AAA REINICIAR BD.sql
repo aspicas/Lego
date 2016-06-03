1c4e44e6bec1964c81b067571cb0e408f986c3b3
Carlos Velasco <carlosvel9794@gmail.com>
2016-06-03 13:10:14 -0400

    Arreglo decimales en inserts.xls y script para reiniciar BD

---
+++ Scripts/AAA REINICIAR BD.sql
@@ -0,0 +1,15 @@
+--Poner ruta del dir. Scripts en Tools->Preferences->Database->Worksheet
+@"DROP\DROP Index.sql";
+@"DROP\DROP Constraints CH Y UN.sql";
+@"DROP\DROP Constraints FK.sql";
+@"DROP\DROP SEQUENCE.sql";
+@"DROP\DROP Type.sql";
+@"DROP\DROP Table.sql";
+
+@"CREATE\CREATE Type.sql";
+@"CREATE\CREATE Table.sql";
+@"CREATE\CREATE Index.sql";
+@"CREATE\CREATE SEQUENCE.sql";
+
+@"ALTER\ALTER Constraints FK.sql";
+@"ALTER\ALTER Constraints CH Y UN.sql";
