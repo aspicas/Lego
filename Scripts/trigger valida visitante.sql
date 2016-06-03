9db894518ec9c5e5bbd29d52e28dfade8ca23dd8
Carlos Velasco <carlosvel9794@gmail.com>
2016-06-03 18:03:09 -0400

    Arreglo Representante visitante

--- Scripts/trigger valida visitante.sql
+++ Scripts/trigger valida visitante.sql
@@ -5,7 +5,8 @@ CREATE OR REPLACE TRIGGER VALIDAR_EDAD_VISITANTE
 BEFORE INSERT ON CLIENTE_VISITANTE
 FOR EACH ROW
 BEGIN 
-IF (months_between(sysdate,:new.CV_F_NAC)/12) < 18 THEN
-raise_Application_error(30003,'El visitante es menor de edad');
+IF (months_between(sysdate,:new.CV_F_NAC)/12) < 18
+AND :new.cv_fk_represen IS NULL THEN
+raise_Application_error(30003,'El visitante es menor de edad sin representante');
 end if;
 END; 
