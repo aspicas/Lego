616b32061c468423e4c6447b4aaaca10d09510cd
David Garcia <david.a.g.r93@gmail.com>
2016-06-03 19:08:46 -0700

    varios archivos ignorados

---
+++ Scripts/TRIGGERS/triggers_HORARIO_TIENDA.sql
@@ -0,0 +1,23 @@
+/*Valida dia y hora */
+
+CREATE OR REPLACE TRIGGER CH_HORARIO
+BEFORE INSERT OR UPDATE ON TIENDA
+FOR EACH ROW
+BEGIN
+  IF :NEW.TI_HORARIO IS NOT NULL THEN
+        FOR i IN :NEW.TI_HORARIO.FIRST..:NEW.TI_HORARIO.LAST LOOP
+              IF :NEW.TI_HORARIO(i).HR_DIA <> 'LUNES' 
+              AND :NEW.TI_HORARIO(i).HR_DIA <> 'MARTES'
+              AND :NEW.TI_HORARIO(i).HR_DIA <> 'MIERCOLES'
+              AND :NEW.TI_HORARIO(i).HR_DIA <> 'JUEVES'
+              AND :NEW.TI_HORARIO(i).HR_DIA <> 'VIERNES'
+              AND :NEW.TI_HORARIO(i).HR_DIA <> 'SABADO'
+              AND :NEW.TI_HORARIO(i).HR_DIA <> 'DOMINGO' THEN
+                  RAISE_APPLICATION_ERROR(-20000, 'Día Invalido');
+              END IF;
+              IF :NEW.TI_HORARIO(i).HR_INICIO > :NEW.TI_HORARIO(i).HR_FIN THEN
+                  raise_application_error(-20001,'HORA DE INICIO DEBE SER MENOR A HORA FINAL');
+              END IF;              
+        END LOOP;
+    END IF;
+END;
