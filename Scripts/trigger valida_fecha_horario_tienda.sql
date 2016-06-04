a697d577a69114eea74ec4823af08ccf7526125e
Jessica Elberg <elbergjessica@gmail.com>
2016-06-03 22:37:06 -0400

    arreglo v2 trigger de horario de tienda

--- Scripts/trigger valida_fecha_horario_tienda.sql
+++ Scripts/trigger valida_fecha_horario_tienda.sql
@@ -1,11 +1,10 @@
-/*Valida que la hora de inicio sea menor a la hora fin no */
-/* NO PROBADA POR FALTA DE INSERTS -.-*/
+/*Valida que la hora de inicio sea menor a la hora fin no, DA ERROR AL COMPILAR */
 
 create or replace trigger valida_fecha_horario_tienda 
-before create or update on TIENDA
+before insert on TIENDA
 FOR EACH ROW
 BEGIN
-IF to_number(to_char(:NEW.TI_HORARIO.HR_INICIO,HH24)) > to_number(to_char(:NEW.TI_HORARIO.HR_FIN,HH24)) THEN
-RAISE_APPLICATION_ERROR(-20909,'La hora de inicio es mayor a la hora final');
-END IF;
+   IF (:new.TI_HORARIO.HR_INICIO - :new.TI_HORARIO.HR_FIN) < 0 THEN
+   RAISE_APPLICATION_ERROR(-20909,'La hora de inicio es mayor a la hora final');
+   end if;
 END;
