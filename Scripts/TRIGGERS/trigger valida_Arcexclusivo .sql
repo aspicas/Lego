3b10d49613e9d92593e7b94f11cd0c9a6b390e1f
Jessica Elberg <elbergjessica@gmail.com>
2016-06-05 08:14:25 -0400

    Trigger de validacion de maxima compra online, creo que debe tener algun error logico, en imagenes estan los resumenes para agregar a los BLOB de producto

---
+++ Scripts/TRIGGERS/trigger valida_Arcexclusivo .sql
@@ -0,0 +1,10 @@
+/*Valida el arco exclusivo, SIN PROBAR POR NO HABER DATOS*/
+create or replace TRIGGER valida_Arcexclusivo 
+BEFORE insert on VALORACION
+FOR EACH ROW
+BEGIN 
+IF :NEW.VA_FK_FAC_ONLI IS NOT NULL AND :NEW.VA_FK_FAC_TIENDA IS NOT NULL
+THEN 
+RAISE_APPLICATION_ERROR(-200032,'La valoracion solo puede ser hecha por un tipo de factura');
+END IF;
+END;
