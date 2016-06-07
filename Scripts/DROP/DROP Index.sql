9698cd9cbb618d9e5739d13859c8c6629470780c
David Garcia <david.a.g.r93@gmail.com>
2016-06-06 23:40:09 -0700

    arreglo en el drop index

--- Scripts/DROP/DROP Index.sql
+++ Scripts/DROP/DROP Index.sql
@@ -1,81 +1,41 @@
 drop index FK_CLASIFICADO_CCH_FK;
-
 drop index FK_CLASIFICADO_CCP_FK;
-
 drop index FK_CIUDAD_CV_FK;
-
 drop index FK_PAIS_CV_FK;
-
 drop index FK_CLIENTE_VISITANTE_FK;
-
 drop index FK_PRODLOTE_DES_FK;
-
 drop index FK_FACTONLINE_DET_FK;
-
 drop index FK_CLIENTE_DET_FK;
-
 drop index FK_PRODLOTE_DET_FK;
-
 drop index FK_DISPROD_DET_FK;
-
 drop index FK_DETPEDIDO_DF_FK;
-
 drop index FK_CLIENTE_DF_FK;
-
 drop index FK_FACTIENDA_DF_FK;
-
 drop index FK_DISPROD_DIP_FK;
-
 drop index FK_PEDIDO_DP_FK;
-
 drop index FK_DETPEDIDO_DL_FK;
-
 drop index FK_PRODLOTE_DL_FK;
-
 drop index FK_PAIS_DIP_FK;
-
 drop index FK_PRODUCTO_DIP_FK;
-
 drop index FK_ESTADO_CIUDAD_FK;
-
 drop index FK_PAIS_EC_FK;
-
 drop index FK_CIUDAD_FA_FK;
-
 drop index FK_CLIENTE_FON_FK;
-
 drop index FK_FACTPEDIDO_PEDIDO2_FK;
-
 drop index FK_CLIENTE_FTIE_FK;
-
 drop index FK_TIENDA_FT_FK;
-
 drop index FK_LINEA_HP_FK;
-
 drop index FK_PRODUCTO_HP_FK;
-
 drop index FK_CIUDAD_INS_FK;
-
 drop index FK_FABRICA_LP_FK;
-
 drop index FK_TIENDA_PD_FK;
-
 drop index FK_FABRICA_PD_FK;
-
 drop index FK_CLASIFICACION_PR_FK;
-
 drop index FK_PRODUCTO_PREP_FK;
-
 drop index FK_PRODUCTO_PREH_FK;
-
 drop index FK_CIUDAD_TIE_FK;
-
 drop index FK_FECHAS_TOPE_FK;
-
 drop index FK_CLIENTE_TOPE_FK;
-
 drop index FK_FACTIENDA_VA_FK;
-
 drop index FK_FACTONLINE_VA_FK;
-
 drop index FK_HISTORICO_PL_FK;
